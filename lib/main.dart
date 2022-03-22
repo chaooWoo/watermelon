dd57af26fea35fcb0d0e1d5e01bc0a62bd148561
W.Chao <785808933@qq.com>
2022-03-22 17:55:57 +0800

    docs(*)：注释main函数进入主页面前的处理

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'tools/system_tool.dart';
 
 void main() async {
   runApp(MyApp());
+  //设置应用的屏幕方向
   SystemTool.keepPortrait();
 }
 
@@ -15,7 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '合成大瓜',
+      //添加路由监控工具，主要是页面跳转的动画设置
       navigatorObservers: [NavigatorTool()],
+      //进入主页面
       home: HomePage(),
     );
   }
--- lib/tools/system_tool.dart
+++ lib/tools/system_tool.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 
 class SystemTool {
   static void changeStatusBarColor([Color color]) {
+    //设置状态栏的颜色/深色模式
     color ??= Colors.white;
     final style = SystemUiOverlayStyle(
       statusBarColor: color,
@@ -15,6 +16,7 @@ class SystemTool {
   }
 
   static void keepPortrait() {
+    //设置屏幕方向为竖屏
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
